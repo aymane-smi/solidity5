e438e91577701c6bb291e4e2cf21b7e8834edf75
aymane <aymanebel2@outlook.fr>
2022-03-12 14:32:25 +0100

    solidity5

---
+++ wei-ether.sol
@@ -0,0 +1,16 @@
+//PSDX-License-Identifier: MIT
+
+pragma solidity ^0.5.3;
+
+contract wei_ether{
+    uint public oneWei = 1 wei;
+    uint public oneEth = 1 ether;
+
+    function testOneWei() public pure returns(bool){
+        return 1 wei == 1;
+    }
+
+    functiontestOneEther() public pure returns(bool){
+        return 1 ether == 1e18 wei;
+    }
+}
